b4c61fa177b6a2398653582ac4433c348c3a063c
Wenzheng Guo <gwz1805504@gmail.com>
2022-11-16 21:38:43 -0800

    fix call issues

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ edfs_client = EDFSClient()
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/api/ls', methods=['GET'])
 def ls():
     database = request.args.get('database')
@@ -18,8 +19,9 @@ def ls():
         files = {}
     else:
         files = [{"id": name, "name": name, "isDir": True if name.find(".") else False}
-            for name in files]
+                 for name in files]
     return jsonify(files)
+
 
 @app.route('/api/mkdir', methods=['GET'])
 def mkdir():
@@ -37,7 +39,7 @@ def cat():
     return jsonify({"data": data})
 
 
-@app.route('/api/cat', methods=['GET'])
+@app.route('/api/rm', methods=['GET'])
 def rm():
     database = request.args.get('database')
     path = request.args.get('path')
@@ -59,7 +61,15 @@ def put():
 def getPartitionLocations():
     database = request.args.get('database')
     path = request.args.get('path')
-    return jsonify({"success": True})
+    data = edfs_client.getPartitionLocations(database, path)
+    response = {}
+    if data is None:
+        response["success"] = False
+    else:
+        response["success"] = True
+        response["data"] = data
+    return jsonify(response)
+
 
 @app.route('/api/readPartition', methods=['GET'])
 def readPartition():
@@ -74,6 +84,7 @@ def readPartition():
         response["success"] = True
         response["data"] = location_list
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run()
